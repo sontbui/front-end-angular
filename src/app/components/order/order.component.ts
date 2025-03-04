dfd37ad1f8ea099476f924007546df48a6dc2d6f
sontbui <sonbuithanh306@gmail.com>
2025-03-04 20:52:55 +0700

    Refactor Admin UI

--- src/app/components/order/order.component.ts
+++ src/app/components/order/order.component.ts
@@ -28,12 +28,12 @@ import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http
     FooterComponent,
     HeaderComponent,
     CommonModule,
-    FormsModule,    
+    FormsModule,
     ReactiveFormsModule,
   ]
 })
 
-export class OrderComponent implements OnInit{
+export class OrderComponent implements OnInit {
   private couponService = inject(CouponService);
   private cartService = inject(CartService);
   private productService = inject(ProductService);
@@ -42,66 +42,65 @@ export class OrderComponent implements OnInit{
   private formBuilder = inject(FormBuilder);
   private router = inject(Router);
 
-  orderForm: FormGroup; // Đối tượng FormGroup để quản lý dữ liệu của form
+  orderForm: FormGroup;
   cartItems: { product: Product, quantity: number }[] = [];
-  totalAmount: number = 0; // Tổng tiền
-  couponDiscount: number = 0; //số tiền được discount từ coupon
+  totalAmount: number = 0;
+  couponDiscount: number = 0;
   couponApplied: boolean = false;
   cart: Map<number, number> = new Map();
   orderData: OrderDTO = {
-    user_id: 0, // Thay bằng user_id thích hợp
-    fullname: '', // Khởi tạo rỗng, sẽ được điền từ form
-    email: '', // Khởi tạo rỗng, sẽ được điền từ form    
-    phone_number: '', // Khởi tạo rỗng, sẽ được điền từ form
-    address: '', // Khởi tạo rỗng, sẽ được điền từ form
+    user_id: 0,
+    fullname: '',
+    email: '',
+    phone_number: '',
+    address: '',
     status: 'pending',
-    note: '', // Có thể thêm trường ghi chú nếu cần
-    total_money: 0, // Sẽ được tính toán dựa trên giỏ hàng và mã giảm giá
-    payment_method: 'cod', // Mặc định là thanh toán khi nhận hàng (COD)
-    shipping_method: 'express', // Mặc định là vận chuyển nhanh (Express)
-    coupon_code: '', // Sẽ được điền từ form khi áp dụng mã giảm giá
+    note: '',
+    total_money: 0,
+    payment_method: 'cod',
+    shipping_method: 'express',
+    coupon_code: '',
     cart_items: []
   };
 
   constructor() {
-    // Tạo FormGroup và các FormControl tương ứng
+
     this.orderForm = this.formBuilder.group({
-      fullname: ['', [Validators.required, Validators.minLength(2)]], // fullname là FormControl bắt buộc      
-      email: ['', [Validators.email]], // Sử dụng Validators.email cho kiểm tra định dạng email
-      phone_number: ['', [Validators.required, Validators.minLength(10), Validators.maxLength(11), Validators.pattern("^[0-9]+$")]], // phone_number bắt buộc và ít nhất 6 ký tự
-      address: ['', [Validators.required, Validators.minLength(5)]], // address bắt buộc và ít nhất 5 ký tự
+      fullname: ['', [Validators.required, Validators.minLength(2)]],
+      email: ['', [Validators.email]],
+      phone_number: ['', [Validators.required, Validators.minLength(10), Validators.maxLength(11), Validators.pattern("^[0-9]+$")]],
+      address: ['', [Validators.required, Validators.minLength(5)]],
       note: [''],
       couponCode: [''],
       shipping_method: ['express'],
       payment_method: ['cod']
     });
   }
-  
-  ngOnInit(): void {  
+
+  ngOnInit(): void {
     debugger
     //this.cartService.clearCart();
-    this.orderData.user_id = this.tokenService.getUserId();    
-    // Lấy danh sách sản phẩm từ giỏ hàng
+    this.orderData.user_id = this.tokenService.getUserId();
+
     debugger
     this.cart = this.cartService.getCart();
-    const productIds = Array.from(this.cart.keys()); // Chuyển danh sách ID từ Map giỏ hàng    
+    const productIds = Array.from(this.cart.keys());
 
-    // Gọi service để lấy thông tin sản phẩm dựa trên danh sách ID
-    debugger    
-    if(productIds.length === 0) {
+
+    debugger
+    if (productIds.length === 0) {
       return;
-    }    
+    }
     this.productService.getProductsByIds(productIds).subscribe({
-      next: (apiResponse: ApiResponse) => {            
+      next: (apiResponse: ApiResponse) => {
         debugger
         const products: Product[] = apiResponse.data
-        // Lấy thông tin sản phẩm và số lượng từ danh sách sản phẩm và giỏ hàng
         this.cartItems = productIds.map((productId) => {
           debugger
           const product = products.find((p) => p.id === productId);
           if (product) {
             product.thumbnail = `${environment.apiBaseUrl}/products/images/${product.thumbnail}`;
-          }          
+          }
           return {
             product: product!,
             quantity: this.cart.get(productId)!
@@ -117,7 +116,7 @@ export class OrderComponent implements OnInit{
         debugger;
         console.error(error?.error?.message ?? '');
       }
-    });        
+    });
   }
   isFormValid(): boolean {
     const fieldsToCheck = ['fullname', 'email', 'phone_number', 'address'];
@@ -128,31 +127,28 @@ export class OrderComponent implements OnInit{
   }
   placeOrder() {
     debugger
+    alert(JSON.stringify(this.orderForm.value));
     if (this.orderForm.errors == null && this.isFormValid()) {
-      // Gán giá trị từ form vào đối tượng orderData
-      /*
-      this.orderData.fullname = this.orderForm.get('fullname')!.value;
-      this.orderData.email = this.orderForm.get('email')!.value;
-      this.orderData.phone_number = this.orderForm.get('phone_number')!.value;
-      this.orderData.address = this.orderForm.get('address')!.value;
-      this.orderData.note = this.orderForm.get('note')!.value;
-      this.orderData.shipping_method = this.orderForm.get('shipping_method')!.value;
-      this.orderData.payment_method = this.orderForm.get('payment_method')!.value;
-      */
-      // Sử dụng toán tử spread (...) để sao chép giá trị từ form vào orderData
+      this.calculateTotal();
       this.orderData = {
         ...this.orderData,
-        ...this.orderForm.value
+        ...this.orderForm.value,
+        total_money: this.totalAmount,
       };
       this.orderData.cart_items = this.cartItems.map(cartItem => ({
         product_id: cartItem.product.id,
         quantity: cartItem.quantity
       }));
-      this.orderData.total_money =  this.totalAmount;
-      // Dữ liệu hợp lệ, bạn có thể gửi đơn hàng đi
+
+      if (!this.totalAmount || this.totalAmount <= 0) {
+        console.error('Invalid total money:', this.totalAmount);
+        alert('Invalid total money! Please check your cart.');
+        return;
+      }
+      this.orderData.total_money = this.totalAmount;
       this.orderService.placeOrder(this.orderData).subscribe({
         next: (response: ApiResponse) => {
-          debugger;          
+          debugger;
           console.error('Order successfully');
           alert('Order successfully');
           this.cartService.clearCart();
@@ -165,38 +161,41 @@ export class OrderComponent implements OnInit{
         error: (error: HttpErrorResponse) => {
           debugger;
           console.error(`Error: ${error?.error?.message ?? ''}`);
+          console.error('Full error response:', error);
+          console.error(`Error message: ${error?.error?.message ?? 'No message'}`);
           alert('Lost information. Please check information again !!!');
         },
       });
     } else {
-      // Hiển thị thông báo lỗi hoặc xử lý khác
-      console.error('Lost information. Please check information !!!');
+      console.error('MEET SOME ERRORRR!!!');
       alert('Error');
-    }        
+    }
   }
-    
+
   decreaseQuantity(index: number): void {
     if (this.cartItems[index].quantity > 1) {
       this.cartItems[index].quantity--;
-      // Cập nhật lại this.cart từ this.cartItems
       this.updateCartFromCartItems();
       this.calculateTotal();
     }
   }
-  
+
   increaseQuantity(index: number): void {
-    this.cartItems[index].quantity++;   
-    // Cập nhật lại this.cart từ this.cartItems
+    this.cartItems[index].quantity++;
     this.updateCartFromCartItems();
     this.calculateTotal();
-  }    
+  }
 
-  // Hàm tính tổng tiền
   calculateTotal(): void {
-      this.totalAmount = this.cartItems.reduce(
-          (total, item) =>  total + (item.product.price * 0.95)  * item.quantity,
-          0
-      );
+    this.totalAmount = this.cartItems.reduce((total, item) => {
+      return total + item.product.price * item.quantity;
+    }, 0);
+    if (!this.totalAmount || this.totalAmount <= 0) {
+      this.totalAmount = 0;
+      console.error('Invalid total money:', this.totalAmount);
+      alert('Invalid total money! Please check your cart.');
+      return;
+    }
   }
   confirmDelete(index: number): void {
     if (confirm('Do you want to remove this?')) {
